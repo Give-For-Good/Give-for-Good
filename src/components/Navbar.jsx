4acc5314d8063cc4bd9812e68a33002af6c88c86
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-08-01 11:22:57 +0300

    fixed import error in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import { close, g, menu } from '../assets';
 import { Link } from "react-router-dom";
 import { navLinks } from '../constants';
@@ -9,7 +8,7 @@ const Navbar = () => {
 
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
-          {/* <img src={g} alt="giveforgood" className="w-[124px] h-[32px]"/>  */}
+          <img src={g} alt="giveforgood" className="w-[124px] h-[32px]"/> 
             <p className="font-bold text-xl text-white">GiveForFood</p>
           <ul className="list-none sm:flex hidden justify-end items-center flex-1">
             {navLinks.map((nav, index) =>(
