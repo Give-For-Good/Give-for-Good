a709dbd0ee095c3c8c77db2b991b16487a7eb751
Gabriel D Minzemalulu <gabe@GabrielsMacBook.lan>
2023-08-02 10:19:54 +0300

    modified home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,57 +1,57 @@
-import styles from '../style';
-import { discount, robot, shield } from '../assets';
-import GetStarted from './GetStarted';
+import styles from "../style";
+import { discount, robot, shield } from "../assets";
+import GetStarted from "./GetStarted";
 // issue of using small letter discount
 // is still a mystery for myself
 
 const Hero = () => {
-    return (
-        <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-         <div className={`flex-1 ${styles.flexStart} flex-col xl:px-16 sm:px-16 px-6`}>
-           
-            {/* ================================================================ Big superhero texts starts here */}
+  return (
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      {/* Left */}
+      <div
+        className="flex-col sm:px-16 px-5 w-1/2"
+      >
+        <div className="flex flex-col">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+            Give For Good
+            <span className="text-gradient">
+              <br /> Transparent 
+            </span>{" "}
+            <span className="text-gradient">
+              <br />Crowdfunding Platform
+            </span>{" "}
+          </h1>
+        </div>
 
-             <div className='flex flex-row justify-between items-centre w-full'>
-                <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] '>
-                    give for good 
-                    <span className='text-gradient'>
-                    <br/> {" "}
-                    web 3 charity
-                    </span> {" "}
-                    {/* infomation system. */}
-                    </h1>
-             </div>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 font-poppins`}>
+          Designed to be the most secure and most credible informantion system,
+          better than the Tanzania interbanking banking system, as it should
+          be...
+        </p>
+      </div>
 
-                  {/* ================================================================ Big superhero texts ends here */}
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
 
-            
+        <img
+          src={shield}
+          alt="robot"
+          className="w-[400px] h-[400px] mt-2 relative z-[5]"
+        />
+        <div className="absolute z-[0] h-[35%] w-[40%] top-0 pink__gradient" />
+        <div className="absolute z-[1] h-[85%] w-[80%] bottom-40 rounded-full white__gradient" />
+        <div className="absolute z-[0] h-[50%] w-[50%] right-20 bottom-20 blue__gradient" />
+      </div>
 
-            <p className={`${styles.paragraph} max-w-[470px] mt-5 font-poppins`}>
-            Designed to be the most secure and most credible informantion system,
-            better than the Tanzania interbanking banking system, as it should be...</p>
-            </div>
-
-            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-                {/* This is where we define our gradients */}
-
-                <img src={shield} alt="robot" className='w-[400px] h-[400px] mt-2 relative z-[5]' />
-                <div className='absolute z-[0] h-[35%] w-[40%] top-0 pink__gradient' />
-                <div className='absolute z-[1] h-[85%] w-[80%] bottom-40 rounded-full white__gradient' />
-                <div className='absolute z-[0] h-[50%] w-[50%] right-20 bottom-20 blue__gradient' />
-            </div>
-             
-             <div className={`ss:hidden ${styles.flexCenter} mt-5`}>
-                <GetStarted />
-             </div>
-            
-            
-        </section>
-    )
-}
+      <div className={`ss:hidden ${styles.flexCenter} mt-5`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
-
-
-
-// issue of using small letter discount
-// is still a mystery for myself  
