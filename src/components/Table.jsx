c705c67178500779db3974b5b6a557fb333017a1
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-08-03 10:30:57 +0300

    modified users table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -151,7 +151,7 @@ const Table = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow w-full`}>
       <div className="flex-1 flex flex-col bg-black-gradient-2">
-      <h2 className={styles.heading2}>Applicants for Fund.</h2>
+      {/* <h2 className={styles.heading2}>Applicants for Fund.</h2> */}
       <div className="p-2 w-full flex flex-col items-center">
       
 {/*Show loading indicator*/}
