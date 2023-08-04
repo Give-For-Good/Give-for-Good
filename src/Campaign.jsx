f748c125019ed5ee3794549464a851888c1a1093
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-08-04 14:30:06 +0300

    code cleanup

--- src/Campaign.jsx
+++ src/Campaign.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import styles from "./style";
 
-import {
-  Navbar,
-  Hero,
-  CardDeal,
-  Business,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-} from "./components";
+import { Navbar, Business, Footer } from "./components";
 import ListTable from "./components/ListTable";
 
 const Campaign = () => {
   return (
     <div className="bg-primary w-full overflow-hidden bg-black-gradient">
-        <div className="bg-black px-12">
-          <Navbar />
-        </div>
+      <div className="bg-black px-12">
+        <Navbar />
+      </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Business />
