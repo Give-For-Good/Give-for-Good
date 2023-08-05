f8944d72b0de160e1d68cd27b1bf47c3b0581523
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-08-05 11:12:29 +0300

    remove unused elements

--- src/Campaign.jsx
+++ src/Campaign.jsx
@@ -1,34 +1,22 @@
 import React from "react";
 import styles from "./style";
 
-import {
-  Navbar,
-  Hero,
-  CardDeal,
-  Business,
-  FeatureCard,
-  FeedbackCard,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-} from "./components";
-import ListTable from "./components/ListTable";
+import { Navbar, Clients, CTA, Footer } from "./components";
 
 const Campaign = () => {
   return (
     <div className="bg-primary w-full overflow-hidden bg-black-gradient">
-        <div className="bg-black px-12">
-          <Navbar />
-        </div>
+      <div className="bg-black px-12">
+        <Navbar />
+      </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <CTA/>
-          <Clients/>
-          <Clients/>
-          <Clients/>
-          <Clients/>
-          <Clients/>
+          <CTA />
+          <Clients />
+          <Clients />
+          <Clients />
+          <Clients />
+          <Clients />
           <Footer />
         </div>
       </div>
